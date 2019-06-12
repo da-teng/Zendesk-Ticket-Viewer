9d0db18d586fe2924d0a1e356fe2229b59ade143
Da Teng <s3674696@student.rmit.edu.au>
2019-06-12 20:16:55 +1000

    add moment js

--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -1,11 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import moment from "moment";
 const Ticket = props => {
   return (
     <div className="ticket">
       <div className="ticket__header">Your Ticket Details</div>
-      <div className="ticket__date">{props.date}</div>
+      <div className="ticket__date">
+        Created{" "}
+        {moment(props.date)
+          .startOf("month")
+          .from()}
+      </div>
       <div className="ticket__status">Status: {props.status}</div>
       <div className="ticket__subject">{props.subject}</div>
       <div className="ticket__description">{props.description}</div>
